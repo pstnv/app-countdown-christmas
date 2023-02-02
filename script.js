84d64b73c43c4acbbdf9ae4fb76f87ac8b99f33e
pstnv <pstnv@mail.ru>
2023-02-02 20:31:13 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const button = document.querySelector('#myButton');
+button.addEventListener('click', () => {
+    document.querySelector('#myAudio').play();
+})
+
+
+function christmasCountdown () {
+    const christmasDate = new Date("December 25, 2022 00:00");
+    const now = new Date();
+    
+    const diff = christmasDate - now;
+
+    const msInSecond = 1000; // сколько мс в секунде
+    const msInMinute = 60 * 1000; // сколько мс в минуте
+    const msInHour = 60 * 60 * 1000;  // сколько мс в часе
+    const msInDay = 24 * 60 * 60 * 1000; // сколько мс в сутках
+
+    const displayDay = Math.floor(diff / msInDay);
+    document.querySelector('.days').textContent = displayDay;
+
+    const displayHour = Math.floor((diff%msInDay) / msInHour);
+    document.querySelector('.hours').textContent = displayHour;
+
+    const displayMinute = Math.floor((diff%msInHour) / msInMinute);
+    document.querySelector('.minutes').textContent = displayMinute;
+
+    const displaySecond = Math.floor((diff%msInMinute) / msInSecond);
+    document.querySelector('.seconds').textContent = displaySecond;
+
+    if (diff <=0) {
+        document.querySelector('.days').textContent = '0';
+        document.querySelector('.hours').textContent = '0';
+        document.querySelector('.minutes').textContent = '0';
+        document.querySelector('.seconds').textContent = '0';
+        clearInterval(timerID);
+        merryChristmas();
+    }
+}
+
+
+let timerID = setInterval(christmasCountdown, 1000);
+
+function merryChristmas () {
+    heading = document.querySelector('h1');
+    heading.textContent = 'MERRI CHRISTMAS!!! HO-HO-HO!';
+    heading.classList.add('red');
+}
